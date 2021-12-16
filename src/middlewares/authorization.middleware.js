e6095b81d19090955ae765ac42400d29c8cd925e
Henrique Marques <henrique@marquesfernandes.com>
2021-12-16 02:55:43 -0300

    feat(deel-test): improved docs

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,6 +1,7 @@
 
 const authorizationMiddleware = async (req, res, next) => {
   const {ProfileService} = req.app.get('services');
+  // TODO: We could improve this to reduce database queries
   const profile = await ProfileService.getById(req.get('profile_id') || 0);
   if (!profile) return res.status(401).send('401 Unauthorized');
 
